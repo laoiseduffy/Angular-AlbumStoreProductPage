e9e3bb0039d83b7b61757d1cb690e0edf3cf0305
n1508693 <laoise.duffy@libertymutual.com>
2019-07-29 10:06:43 -0400

    Getting the album info from the album.json

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
 
-  private albumUrl = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
-  constructor( private http: Http) { }
+  constructor( private _http: Http) { }
 
   getAlbum(id: number) {
-    return this.http.get(this.albumUrl).map((response) => response.json());
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
